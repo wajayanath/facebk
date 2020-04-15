703e3bc0c1294815f06deaf3e436a427ff3d6541
wajayanath <wajayanath@gmail.com>
2020-04-15 13:01:15 +0530

    User can like posts test

---
+++ app/Http/Resources/Like.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Like extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => [
+                'type' => 'likes',
+                'like_id' => $this->id,
+                'attributes' => []
+            ],
+            'links' => [
+                'self' => url('/posts/'.$this->pivot->post_id),
+
+            ]
+        ];
+    }
+}
--- resources/js/store/modules/posts.js
+++ resources/js/store/modules/posts.js
@@ -1,6 +1,7 @@
 const state = {
     newsPosts:null,
     newsPostsStatus: null,
+    postMessage: '',
 };
 
 const getters = {
@@ -11,6 +12,9 @@ const getters = {
         return {
             newsPostsStatus: state.newsPostsStatus,
         }
+    },
+    postMessage: state => {
+        return state.postMessage;
     }
 }
 
@@ -25,7 +29,17 @@ const actions = {
             .catch(error => {
                 commit('setPostsStatus', 'error');
             });
-    }
+    },
+    postMessage({commit, state}) {
+        commit('setPostsStatus', 'loading');
+        axios.post('/api/posts',{ body:state.postMessage })
+            .then(res => {
+                commit('pushPost', res.data);
+                commit('updateMessage', '');
+            })
+            .catch(error => {
+            });
+    },
 };
 
 const mutations = {
@@ -34,6 +48,12 @@ const mutations = {
     },
     setPostsStatus(state, status) {
         state.newsPostsStatus = status;
+    },
+    updateMessage(state, message) {
+        state.postMessage = message;
+    },
+    pushPost(state, post) {
+        state.newsPosts.data.unshift(post);
     }
 };
 
