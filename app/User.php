8fbad15ae3f03dd78b0c7b4d025361cd87bb9e4b
wajayanath <wajayanath@gmail.com>
2020-04-21 17:01:13 +0530

    Upload User Image Test

--- app/User.php
+++ app/User.php
@@ -38,6 +38,25 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function images()
+    {
+        return $this->hasMany(UserImage::class);
+    }
+
+    public function coverImage()
+    {
+        return $this->hasOne(UserImage::class)
+            ->orderByDesc('id')
+            ->where('location', 'cover');
+    }
+
+    public function profileImage()
+    {
+        return $this->hasOne(UserImage::class)
+            ->orderByDesc('id')
+            ->where('location', 'profile');
+    }
+
     public function LikedPosts()
     {
         return $this->belongsToMany(Post::class, 'likes', 'user_id', 'post_id');
